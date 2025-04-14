94999928da921a9530199b482d0da354949ff108
IanMilli <120601739+IanMilli@users.noreply.github.com>
2025-04-14 18:05:07 +0100

    maDE EDITORIAL CHANGES

--- javascript/index.js
+++ javascript/index.js
@@ -69,6 +69,9 @@ window.onload = function () {
 /*animation js*/
 // All animations will take twice the time to accomplish
 document.documentElement.style.setProperty('--animate-duration', '7s');
-
-/*script for email service*/
-
+//map js
+document.addEventListener('DOMContentLoaded', async () => {
+    await customElements.whenDefined('gmpx-store-locator');
+    const locator = document.querySelector('gmpx-store-locator');
+    locator.configureFromQuickBuilder(CONFIGURATION);
+  });
